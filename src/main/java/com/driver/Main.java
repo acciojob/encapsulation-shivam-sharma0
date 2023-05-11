2b3e0769c678ad9a38e5e2855d624159d0aa337e
shivam-sharma0 <vashistshivam931@gmail.com>
2023-05-11 12:00:33 +0530

    Sixth

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         RWOnly first= new RWOnly();
 //        first.name="Shivam";
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,6 @@
 package com.driver;
-class RWOnly{
+
+public class RWOnly{
   private String name;
 
     public String getName() {
